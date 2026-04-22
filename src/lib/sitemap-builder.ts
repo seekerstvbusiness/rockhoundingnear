3748bba7b44dd5bd9ade95907e72cbc2cbe79d43
seekerstvbusiness <seekerstv.business@gmail.com>
2026-04-23 00:12:59 +0530

    Restore XSL stylesheet to sitemaps (accidentally removed)

--- src/lib/sitemap-builder.ts
+++ src/lib/sitemap-builder.ts
@@ -36,7 +36,7 @@ export function buildSitemapIndex(sitemaps: Array<{ loc: string; lastmod: string
     )
     .join('\n')
 
-  return `<?xml version="1.0" encoding="UTF-8"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${entries}\n</sitemapindex>`
+  return `<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>\n<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n${entries}\n</sitemapindex>`
 }
 
 export function buildSitemap(urls: SitemapUrl[]): string {
@@ -63,7 +63,7 @@ export function buildSitemap(urls: SitemapUrl[]): string {
     })
     .join('\n')
 
-  return `<?xml version="1.0" encoding="UTF-8"?>\n<urlset\n  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n${entries}\n</urlset>`
+  return `<?xml version="1.0" encoding="UTF-8"?>\n<?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>\n<urlset\n  xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"\n  xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">\n${entries}\n</urlset>`
 }
 
 export const XML_HEADERS = {
